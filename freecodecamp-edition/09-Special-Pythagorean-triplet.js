3b88a66a0004b788ff035924287a16f1f6b38f38
Ildar Nogmanov <nogmanov@gmail.com>
2020-05-07 19:26:38 +0300

    Remove uselessness

--- freecodecamp-edition/09-Special-Pythagorean-triplet.js
+++ freecodecamp-edition/09-Special-Pythagorean-triplet.js
@@ -1,15 +1,8 @@
 function specialPythagoreanTriplet(n) {
-  let toLoop = true;
-
-  for (let a = 1; toLoop && a < n; a++) {
-    for (let b = a + 1; toLoop && a + b < n; b++) {
-      for (
-        let c = b + 1;
-        a < b && b < c && a < c && a + b + c <= n && toLoop;
-        c++
-      ) {
+  for (let a = 1; a < n; a++) {
+    for (let b = a + 1; a + b < n; b++) {
+      for (let c = b + 1; a < b && b < c && a < c && a + b + c <= n; c++) {
         if (a * a + b * b == c * c && a + b + c == n) {
-          toLoop = false;
           return a * b * c;
         }
       }
